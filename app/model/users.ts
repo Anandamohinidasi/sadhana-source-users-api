7d7a9b7956beddcf921bcef94b289711262e5e81
ananda rodrigues <anandarodrigues@anandas-MacBook-Pro.local>
2021-10-09 23:26:13 -0700

    Add user model middleware to encrypt password before saving to db

--- app/model/users.ts
+++ app/model/users.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 export type UsersDocument = mongoose.Document & {
   name: string,
   document: string,
   email: string,
   spiritualName: string,
+  password: string,
   guru: string,
   status_id: string,
   permissions: string[]
@@ -15,12 +17,20 @@ const usersSchema = new mongoose.Schema({
   document: String,
   email: String,
   spiritualName: String,
+  password: String,
   guru: String,
   status_id: {type: String, default: "61612b0d6fa4d7ac743be63c"},
   permissions: [String]
 });
 
-// Note: OverwriteModelError: Cannot overwrite `Books` model once compiled. error
+usersSchema.pre("save", async function(next) {
+const user: any = this;
+if(user.isModified("password")) {
+  user.password = await bcrypt.hash(user.password, 8);
+}
+  next();
+})
+
 export const users = (mongoose.models.permissions ||
 mongoose.model<UsersDocument>('users', usersSchema, process.env.DB_USERS_COLLECTION)
 );
