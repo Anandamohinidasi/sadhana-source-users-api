9c6bdce3e834d89291014723d72ac0749ee2bc95
ananda rodrigues <anandarodrigues@anandas-MacBook-Pro.local>
2021-10-10 19:00:42 -0700

    Simply environments

--- app/handler.ts
+++ app/handler.ts
@@ -1,11 +1,7 @@
 
 import { Handler, Context } from 'aws-lambda';
 import dotenv from 'dotenv';
-import path from 'path';
-const dotenvPath = path.join(__dirname, '../', `config/.env.${process.env.NODE_ENV}`);
-dotenv.config({
-  path: dotenvPath,
-});
+dotenv.config();
 
 import { users } from './model';
 import { permissions } from './model';
